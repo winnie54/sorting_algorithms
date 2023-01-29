9001c24f64bd6c4680ead557bce106c704eacac2
winnie54 <winnieeluu54@gmail.com>
2023-01-29 01:22:53 -0800

    header file

---
+++ sort.h
@@ -0,0 +1,27 @@
+#ifndef _SORT_H_
+#define _SORT_H_
+
+#include <stdio.h>
+/**
+ * struct listnt_s - Doubly linked list node
+ * @n: integer sorted on the node 
+ * @prev: pointer to the previous element of the list
+ * @next: pointer to the next element of the list
+ */
+typedef struct listint_s
+{
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
+}listint_t;
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+
+#endif
